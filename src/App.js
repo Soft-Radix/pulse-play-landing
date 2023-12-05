3d2f01a956c027894abff8d34a6a30c2aaba1e08
lodhikomal <komallodhi720@gmail.com>
2023-12-05 18:50:48 +0530

    commentfooter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Footer from "./component/Footer";
+// import Footer from "./component/Footer";
 import Header from "./component/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HeroSection from "./pages/HeroSection";
